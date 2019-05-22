7b746515555ff01f9a8aecfa5fa3673da36153ec
Valarie Adams <deviantdear@gmail.com>
2019-05-21 22:10:18 -0700

    Added some Character scripts
    These are from a prveious project and I will modify them to fit our needs but they are a good foundation for out character scripts

---
+++ SkwiggleTower/Assets/Scripts/CharacterScripts/CharacterStats.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxhealth = 100;
+    public int currentHealth { get; private set; } //any class can get the value only change it in this class
+    public int xp { get; private set; }
+
+    public Stat damage;
+    public Stat armor;
+
+    public event System.Action<int, int> OnHealthChanged;
+
+    private void Awake()
+    {
+        currentHealth = maxhealth; //initial value
+        xp = 5;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (this != null)
+        {
+            damage -= armor.GetValue();
+            damage = Mathf.Clamp(damage, 0, int.MaxValue); //prevents negative damage values 
+
+            currentHealth -= damage;
+            //Debug.Log(transform.name + " takes " + damage + " damage.");
+
+            OnHealthChanged?.Invoke(maxhealth, currentHealth);
+
+            if (currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    public void Heal(int healing)
+    {
+        currentHealth += Mathf.Clamp(healing, 1, maxhealth); //prevents excessive Health
+
+        OnHealthChanged?.Invoke(maxhealth, currentHealth);
+    }
+
+    public void gainXP(int _xp) //Gain XP
+    {
+        xp += _xp;
+    }
+
+    public virtual void Die()
+    {
+        //Die in some way, should be overriden
+        // Debug.Log(transform.name + " died.");
+
+    }
+}
---
+++ SkwiggleTower/Assets/Scripts/CharacterScripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Transform target;
+    //NavMeshAgent agent;
+    private float rotSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+          
+            FaceTarget();
+        }
+    }
+
+    public void Movement()
+    {
+        
+    }
+
+    public void FollowTarget(Interactable newTarget)
+    {
+       
+
+        target = newTarget.interactionTransform;
+    }
+
+    public void stopFollowing()
+    {
+     
+        target = null;
+    }
+
+    public void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z)); //prevents player from looking up and down
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotSpeed);
+
+    }
+}
