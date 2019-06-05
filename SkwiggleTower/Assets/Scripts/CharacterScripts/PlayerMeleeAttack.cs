5c28cd1218b480b2256231a5274da42817bcc30d
JakeHresh <jakeyhresh123@gmail.com>
2019-06-05 15:24:50 -0700

    Added test Player Melee attack script as well as a test enemy script, which was attatched to a test enemy

---
+++ SkwiggleTower/Assets/Scripts/CharacterScripts/PlayerMeleeAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMeleeAttack : MonoBehaviour
+{
+    private float attackCooldown;
+    public float startAttackCooldown;
+
+    public Transform attackPos;
+    public LayerMask whatAreEnemies;
+    public float attackRange;
+    public int damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //once 0, player can attack
+        if (attackCooldown <= 0)
+        {
+            if(Input.GetKey(KeyCode.Space))
+            {
+                Collider2D[] enemiesInRange = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatAreEnemies);
+                for(int i = 0; i < enemiesInRange.Length; i++)
+                {
+                    enemiesInRange[i].GetComponent<JacobEnemyTest>().takeDamage(damage);
+                }
+            }
+            attackCooldown = startAttackCooldown;
+        }
+        else
+        {
+            attackCooldown -= Time.deltaTime;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+
+}
---
+++ SkwiggleTower/Assets/Scripts/EnemyScripts/JacobEnemyTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterStats))]
+public class JacobEnemyTest : Interactable
+{
+    PlayerManager playerManager; //References player
+    CharacterStats myStats; //References Enemy Stats
+    public int health;
+
+    void Start()
+    {
+        playerManager = PlayerManager.instance; //uses the singleton instance to reference the player manager
+        myStats = GetComponent<CharacterStats>();
+        xpGained = 100;
+    }
+
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        Combat playerCombat = playerManager.player.GetComponent<Combat>(); //Attack enemy
+        if (playerCombat != null)
+        {
+            playerCombat.Attack(myStats); //Gets reference to damage modifier to attack 
+        }
+    }
+
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("'Tis but a flesh wound");
+    }
+
+}
