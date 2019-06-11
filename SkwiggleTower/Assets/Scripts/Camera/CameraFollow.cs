85e2d4119c0a2315c1fc8f5620f0a16e0e480c22
Valarie Adams <deviantdear@gmail.com>
2019-06-10 23:43:29 -0700

    Added camera zoom per the design doc
    Updated CameraFollow Script to have zoom in and out with the scroll wheel

--- SkwiggleTower/Assets/Scripts/Camera/CameraFollow.cs
+++ SkwiggleTower/Assets/Scripts/Camera/CameraFollow.cs
@@ -2,21 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Val Script: Camera Zoom and Follow player
+/// </summary>
 public class CameraFollow : MonoBehaviour
 {
+    //Follow Camera  
     public float dampTime = 0.15f;
     private Vector3 velocity = Vector3.zero;
+    private float offset = 0f; 
     public Transform target;
 
-    // Update is called once per frame
+
+    //Zoom feature
+    Camera cam;
+    public float zoomSpeed = 4f;
+    private float currentZoom = 1f;
+    public float minZoom = 3f;
+    public float maxZoom = 7f;
+
+
+    public void Start()
+    {
+        cam = this.GetComponent<Camera>();
+    }
+
     void Update()
+    {
+        //Gets input from scroll wheel
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
     {
         if (target)
         {
+            //Follow Character
             Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
-            Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
             Vector3 destination = transform.position + delta;
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            //Zoom update
+            cam.orthographicSize = offset + currentZoom;
         }
     }
 
