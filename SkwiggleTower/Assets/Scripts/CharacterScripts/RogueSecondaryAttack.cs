4a6a4006d45d441ec8eb300143eca4e7de51ed16
JakeHresh <jakeyhresh123@gmail.com>
2019-06-13 12:44:40 -0700

    Adding additional Raycasts to attempt to prevent wall clipping

--- SkwiggleTower/Assets/Scripts/CharacterScripts/RogueSecondaryAttack.cs
+++ SkwiggleTower/Assets/Scripts/CharacterScripts/RogueSecondaryAttack.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RogueSecondaryAttack : MonoBehaviour
 {
+    //Not all of these variables are used yet. Currently working on getting the dash to work correctly.
     public float attackRangex;
     public float attackRangey;
     public float distanceFromPlayer;
@@ -12,7 +13,10 @@ public class RogueSecondaryAttack : MonoBehaviour
     public float dashDist;
     public float dashSpeed;
     public float ultPowerGenerated;
+    public float wallDetectDist;
     public Transform playerPos;
+    public Transform playerPosBottom;
+    public Transform playerPosTop;
     private float startDashDist;
     private bool isClicked;
     // Start is called before the first frame update
@@ -27,8 +31,12 @@ public class RogueSecondaryAttack : MonoBehaviour
     {
         //detecting platform layer 9
         int layerMask = 1 << 9;
-        RaycastHit2D hit = Physics2D.Raycast(playerPos.position, Vector2.right, dashDist, layerMask);
-        if (hit.collider == null)
+        //Generates raycasts that will be used to detect walls in front of the player from a certain distance
+        //If that distance is shorter than the default dash distance, the player instead travels the differance between the detected platform and the player's current position
+        RaycastHit2D hitMidR = Physics2D.Raycast(playerPos.position, Vector2.right, wallDetectDist, layerMask);
+        RaycastHit2D hitTopR = Physics2D.Raycast(playerPosTop.position, Vector2.right, wallDetectDist, layerMask);
+        RaycastHit2D hitBottomR = Physics2D.Raycast(playerPosBottom.position, Vector2.right, wallDetectDist, layerMask);
+        if (hitMidR.collider == null || hitBottomR.collider == null || hitTopR.collider == null)
         {
             //Attack is activated by right click
             if (Input.GetMouseButtonDown(1) && !isClicked)
@@ -49,17 +57,17 @@ public class RogueSecondaryAttack : MonoBehaviour
         else
         {
             Debug.Log("I see you");
-            float distance = hit.point.x - playerPos.position.x;
+            float distance = hitMidR.point.x - playerPos.position.x;
             if (Input.GetMouseButtonDown(1) && !isClicked)
             {
                 isClicked = true;
             }
-            if (isClicked && distance >= 0)
+            if (isClicked && distance > 1)
             {
                 playerPos.position += new Vector3(dashSpeed, 0, 0);
                 distance -= 1;
             }
-            if (dashDist < 0)
+            if (distance <= 1)
             {
                 dashDist = startDashDist;
                 isClicked = false;
