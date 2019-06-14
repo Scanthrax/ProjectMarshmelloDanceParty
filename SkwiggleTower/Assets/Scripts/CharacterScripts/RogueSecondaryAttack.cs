aef2fe855dcc459547ad5fc87b976719122016e9
JakeHresh <jakeyhresh123@gmail.com>
2019-06-13 23:36:31 -0700

    Implemented a basic attack for dash when dashing right
    Unfortunately, the dash is still a work in progress. Wall clipping is still possible, but minimized due to raycasts pointing left and right, which measure the distance between player and platforms. I also added a basic attack, so that anything behind the player is damaged while dashing. The way the dash currently functions is that it moves fast enough to pass through various game objects. Raycasts can be used to prevent the player from clipping through walls. The main things that remain to be implemented are i-frames for the player passing through enemies and cooldown. Everything else simply requires polish.

--- SkwiggleTower/Assets/Scripts/CharacterScripts/RogueSecondaryAttack.cs
+++ SkwiggleTower/Assets/Scripts/CharacterScripts/RogueSecondaryAttack.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class RogueSecondaryAttack : MonoBehaviour
 {
     //Not all of these variables are used yet. Currently working on getting the dash to work correctly.
-    public float attackRangex;
-    public float attackRangey;
+    public Vector2 attackRange;
+    public int damage;
     public float distanceFromPlayer;
     public float dashCooldown;
-    public float dashDamage;
     public float dashDist;
     public float dashSpeed;
     public float ultPowerGenerated;
     public float wallDetectDist;
+    public Transform attackPos;
     public Transform playerPos;
     public Transform playerPosBottom;
     public Transform playerPosTop;
+    public LayerMask whatAreEnemies;
     private float startDashDist;
     private bool isClicked;
     // Start is called before the first frame update
@@ -33,45 +34,129 @@ public class RogueSecondaryAttack : MonoBehaviour
         int layerMask = 1 << 9;
         //Generates raycasts that will be used to detect walls in front of the player from a certain distance
         //If that distance is shorter than the default dash distance, the player instead travels the differance between the detected platform and the player's current position
+        //Right now, mid raycast is working correctly. Since it's a prototype, it just needs to demonstrate basic unpolished functionality
         RaycastHit2D hitMidR = Physics2D.Raycast(playerPos.position, Vector2.right, wallDetectDist, layerMask);
-        RaycastHit2D hitTopR = Physics2D.Raycast(playerPosTop.position, Vector2.right, wallDetectDist, layerMask);
-        RaycastHit2D hitBottomR = Physics2D.Raycast(playerPosBottom.position, Vector2.right, wallDetectDist, layerMask);
-        if (hitMidR.collider == null || hitBottomR.collider == null || hitTopR.collider == null)
+        RaycastHit2D hitMidL = Physics2D.Raycast(playerPos.position, Vector2.left, wallDetectDist, layerMask);
+        //RaycastHit2D hitTopR = Physics2D.Raycast(playerPosTop.position, Vector2.right, wallDetectDist, layerMask);
+        //RaycastHit2D hitBottomR = Physics2D.Raycast(playerPosBottom.position, Vector2.right, wallDetectDist, layerMask);
+        if(playerPos.localScale == new Vector3(1, 1, 1))//checks if player is facing right
         {
-            //Attack is activated by right click
-            if (Input.GetMouseButtonDown(1) && !isClicked)
+            if (hitMidR.collider == null)
             {
-                isClicked = true;
+                //Attack is activated by right click
+                if (Input.GetMouseButtonDown(1) && !isClicked)
+                {
+                    isClicked = true;
+                }
+                if (isClicked && dashDist >= 0)
+                {
+                    //dashes forward until dashDist depletes
+                    playerPos.position += new Vector3(dashSpeed, 0, 0);
+                    dashDist -= 1;
+                }
+                if (dashDist < 0)
+                {
+                    dashDist = startDashDist;
+                    isClicked = false;
+                }
+                Collider2D[] enemiesInRange = Physics2D.OverlapBoxAll(attackPos.position, attackRange, whatAreEnemies);
+                for (int i = 0; i < enemiesInRange.Length; i++)
+                {
+                    //Uses a method in CharacterStats.cs for enemy to take damage
+                    enemiesInRange[i].GetComponent<CharacterStats>().TakeDamage(damage);
+                    Debug.Log("Got 'em");
+                }
             }
-            if (isClicked && dashDist >= 0)
+            else
             {
-                playerPos.position += new Vector3(dashSpeed, 0, 0);
-                dashDist -= 1;
-            }
-            if (dashDist < 0)
-            {
-                dashDist = startDashDist;
-                isClicked = false;
+                //If wall is detected and distance is less than dashDist, it calculates new distance and dashes that amount to the right
+                //Debug.Log("I see you");
+                float distanceMid = hitMidR.point.x - playerPos.position.x;
+
+                if (Input.GetMouseButtonDown(1) && !isClicked)
+                {
+                    isClicked = true;
+                }
+                if (isClicked && distanceMid > 1)
+                {
+                    playerPos.position += new Vector3(dashSpeed, 0, 0);
+                    distanceMid -= 1;
+                }
+                if (distanceMid <= 1)
+                {
+                    dashDist = startDashDist;
+                    isClicked = false;
+                }
+                Collider2D[] enemiesInRange = Physics2D.OverlapBoxAll(attackPos.position, attackRange, whatAreEnemies);
+                for (int i = 0; i < enemiesInRange.Length; i++)
+                {
+                    //Uses a method in CharacterStats.cs for enemy to take damage
+                    enemiesInRange[i].GetComponent<CharacterStats>().TakeDamage(damage);
+                    Debug.Log("Got 'em");
+                }
             }
         }
-        else
+        else//the case if player is facing left
         {
-            Debug.Log("I see you");
-            float distance = hitMidR.point.x - playerPos.position.x;
-            if (Input.GetMouseButtonDown(1) && !isClicked)
+            if (hitMidL.collider == null)
             {
-                isClicked = true;
+                //Attack is activated by right click
+                if (Input.GetMouseButtonDown(1) && !isClicked)
+                {
+                    isClicked = true;
+                }
+                if (isClicked && dashDist >= 0)
+                {
+                    //dashes left while dashDist is greater than or equal to 0
+                    playerPos.position -= new Vector3(dashSpeed, 0, 0);
+                    dashDist -= 1;
+                }
+                if (dashDist < 0)
+                {
+                    dashDist = startDashDist;
+                    isClicked = false;
+                }
+                Collider2D[] enemiesInRange = Physics2D.OverlapBoxAll(attackPos.position, attackRange, whatAreEnemies);
+                for (int i = 0; i < enemiesInRange.Length; i++)
+                {
+                    //Uses a method in CharacterStats.cs for enemy to take damage
+                    enemiesInRange[i].GetComponent<CharacterStats>().TakeDamage(damage);
+                    Debug.Log("Got 'em");
+                }
             }
-            if (isClicked && distance > 1)
+            else
             {
-                playerPos.position += new Vector3(dashSpeed, 0, 0);
-                distance -= 1;
-            }
-            if (distance <= 1)
-            {
-                dashDist = startDashDist;
-                isClicked = false;
+                //if wall to left is detected, calculates distance and dashes that amount to the left
+                //Debug.Log("I see you");
+                float distanceMid = playerPos.position.x - hitMidR.point.x;
+
+                if (Input.GetMouseButtonDown(1) && !isClicked)
+                {
+                    isClicked = true;
+                }
+                if (isClicked && distanceMid >= -2)
+                {
+                    playerPos.position -= new Vector3(dashSpeed, 0, 0);
+                    distanceMid -= 1;
+                }
+                if (distanceMid < -2)
+                {
+                    dashDist = startDashDist;
+                    isClicked = false;
+                }
+                Collider2D[] enemiesInRange = Physics2D.OverlapBoxAll(attackPos.position, attackRange, whatAreEnemies);
+                for (int i = 0; i < enemiesInRange.Length; i++)
+                {
+                    //Uses a method in CharacterStats.cs for enemy to take damage
+                    enemiesInRange[i].GetComponent<CharacterStats>().TakeDamage(damage);
+                    Debug.Log("Got 'em");
+                }
             }
         }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(attackPos.position, attackRange);
     }
 }
