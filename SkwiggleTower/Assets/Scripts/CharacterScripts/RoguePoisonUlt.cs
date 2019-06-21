7f4f22e2bc78c4d33fe3ec7d710de829961b680d
JakeHresh <jakeyhresh123@gmail.com>
2019-06-20 22:42:18 -0700

    2 new Scripts and Modified DashAttack for poison effect
    In short, RogueUltTestActivation.cs allows you to press p to activate the Rogue's poison ult. RoguePoisonUlt.cs is the script that performs the ult, using the time % 15 to determine when to have the enemy take damage. The addition to RogueSecondaryAttack.cs allows the ult script to be added to an enemy if the ult is activated.

---
+++ SkwiggleTower/Assets/Scripts/CharacterScripts/RoguePoisonUlt.cs
@@ -0,0 +1,34 @@
+﻿//Jacob Hreshchyshyn
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoguePoisonUlt : MonoBehaviour
+{
+    public int damage = 1;
+    public int timer = 180;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer--;
+        if (timer > 0 && timer % 15 == 0)
+        {
+            GetComponent<CharacterStats>().TakeDamage(damage);
+            Debug.Log("Taking damage" + damage);
+        }
+        else if(timer <= 0)
+        {
+           DestroyScriptInstance();
+        }
+    }
+    void DestroyScriptInstance()
+    {
+        Destroy(this);
+    }
+}
