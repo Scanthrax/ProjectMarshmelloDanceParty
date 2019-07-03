4ae89a0a787a173c9a252033555a6fc97f779289
Valarie Adams <deviantdear@gmail.com>
2019-07-02 22:41:50 -0700

    Testing Scriptable Object Ability System

---
+++ SkwiggleTower/Assets/Scripts/CharacterScripts/Ability.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Val Scriptable Object Version of Ability System
+/// Abstract class that should be overriden
+/// Never exists in the hierachy
+/// </summary>
+public abstract class Ability : ScriptableObject
+{
+    public string aName = "New Ability";
+    public Sprite aSprite;
+    public AudioClip aSound;
+    public float aBaseCoolDown;
+
+    public abstract void Initialize(GameObject obj); //replaces Awake or Start
+    public abstract void TriggerAbility();
+
+
+}
---
+++ SkwiggleTower/Assets/Scripts/CharacterScripts/RaycastAbility.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Valarie Script for ability system using scriptable objects
+/// </summary>
+[CreateAssetMenu(menuName ="Abilities")]
+public class RaycastAbility : Ability
+{
+    public int aDamage = 1;
+    public float aRange = 50f;
+    public float hitForce = 100f;
+    public Color aColor = Color.white;
+
+   // private RayCastShootTriggerable rcShoot;
+
+    public override void Initialize(GameObject obj)
+    {
+       // rcShoot = obj.GetComponent<RayCastShootTriggerable>();
+       //set damage, range, and hitforce
+       // set new material 
+       //set color of material
+
+        throw new System.NotImplementedException();
+    }
+
+    public override void TriggerAbility()
+    {
+        //call Fire in Ability object
+    }
+}
