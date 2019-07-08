7dc73b0ec113017451ffb0ae3ad9b8cf7a971020
Valarie Adams <deviantdear@gmail.com>
2019-07-07 22:30:12 -0700

    Update CharacterStats.cs
    added check if max health equals current health

--- SkwiggleTower/Assets/Scripts/CharacterScripts/CharacterStats.cs
+++ SkwiggleTower/Assets/Scripts/CharacterScripts/CharacterStats.cs
@@ -46,6 +46,10 @@ public class CharacterStats : MonoBehaviour
 
     public void Heal(int healing)
     {
+        if(currentHealth == maxHealth)
+        {
+            return;
+        }
         currentHealth += Mathf.Clamp(healing, 1, maxHealth); //prevents excessive Health
 
         OnHealthChanged?.Invoke(maxHealth, currentHealth);
