aa682f949b15a8d6eff72a00ed9835de6fe79259
Ron <rweeden97@gmail.com>
2019-08-02 17:45:27 -0700

    candle

---
+++ SkwiggleTower/Assets/Candle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class Candle : MonoBehaviour
+{
+    public List<Light2D> lights;
+    public float intensity;
+    public float offset;
+    public float speed;
+    List<float> noiseOffset;
+
+    private void Start()
+    {
+        noiseOffset = new List<float>();
+        for (int i = 0; i < lights.Count; i++)
+        {
+            noiseOffset.Add(Random.value * 1000f);
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < lights.Count; i++)
+        {
+            var light = lights[i];
+            light.intensity = intensity + Mathf.PerlinNoise(noiseOffset[i] + Time.time * speed,0f) * offset;
+        }
+    }
+}
